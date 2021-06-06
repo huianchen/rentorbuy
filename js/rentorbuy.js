812d17f0274cb926fcb3666fdae9a7aeee804a18
Anne <37337537+huianchen@users.noreply.github.com>
2021-06-06 17:21:03 +0800

    add js

---
+++ js/rentorbuy.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  
+  $(".buy a").click(function(){
+    $(this).addClass("active");
+    $(".rent a").removeClass("active");
+  });
+
+  $(".rent a").click(function(){
+    $(this).addClass("active");
+    $(".buy a").removeClass("active");
+  });
+
+  $(".condition").click(function(){
+    $(".condition.active").removeClass("active");
+    $(this).toggleClass("active");
+  });
+
+  $(".sort").click(function(){
+    $(this).toggleClass("active");
+  });
+
+  $(".save").click(function(){
+    $(this).toggleClass("active");
+  });
+
+  $(".select a").click(function(){
+    $(this).toggleClass("active");
+    $(".condition").removeClass("active");
+  });
+
+  $(".delete a").click(function(){
+    $(".select a").removeClass("active");
+  });
+
+});
+
+function goBack() {
+  window.history.back();
+}
